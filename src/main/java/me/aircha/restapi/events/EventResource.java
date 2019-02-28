6560c790166f5e92007e82b63a6183d26bca3e9a
aircha <aircha@gmail.com>
2019-03-01 03:30:19 +0900

    make hateoas link

---
+++ src/main/java/me/aircha/restapi/events/EventResource.java
@@ -0,0 +1,14 @@
+package me.aircha.restapi.events;
+
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+class EventResource extends Resource<Event> {
+
+    EventResource(Event event, Link... links) {
+        super(event, links);
+        add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
+    }
+}
