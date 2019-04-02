5ad2eb20aab8e6f34fab33e5ecfce27a27b4c7ae
aircha <aircha@gmail.com>
2019-04-03 02:53:33 +0900

    make Json Serializer

--- src/main/java/me/aircha/restapi/events/Event.java
+++ src/main/java/me/aircha/restapi/events/Event.java
@@ -1,7 +1,9 @@
 package me.aircha.restapi.events;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.*;
 import me.aircha.restapi.accounts.Account;
+import me.aircha.restapi.accounts.AccountSerializer;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -32,6 +34,7 @@ class Event {
     @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
     @ManyToOne
+    @JsonSerialize(using = AccountSerializer.class)
     private Account manager;
 
     void update() {
